a252d7201db5fe7cdfdc9a3bf0f2f5dc37a47201
Quang Nguyen <quangnd@aplus.app>
2024-01-05 12:29:31 +0700

    test

--- models/dhp_target_tbl.sql
+++ models/dhp_target_tbl.sql
@@ -2,11 +2,17 @@
 materialized="table"
 )}}
            
-select
-    'QUANG' as subject,
-    category,
-    patient,
-    _AIRBYTE_AB_ID,
-    _AIRBYTE_EMITTED_AT,
-    NOW() as _AIRBYTE_NORMALIZED_AT
+-- select
+--     'QUANG' as subject,
+--     category,
+--     patient,
+--     _AIRBYTE_AB_ID,
+--     _AIRBYTE_EMITTED_AT,
+--     NOW() as _AIRBYTE_NORMALIZED_AT
+-- from Observation
+
+select 
+    'a' as data1,
+    'b' as data2,
+    id as uid
 from Observation
