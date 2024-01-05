229c52c482837ed30a4c07ca66bb31eed5572b13
Quang Nguyen <quangnd@aplus.app>
2024-01-05 12:06:32 +0700

    test

--- models/EXCHANGE_RATES.sql
+++ models/EXCHANGE_RATES.sql
@@ -8,6 +8,6 @@ select
     patient,
     _AIRBYTE_AB_ID,
     _AIRBYTE_EMITTED_AT,
-    convert_timezone('UTC', current_timestamp()) as _AIRBYTE_NORMALIZED_AT,
+    convert_timezone('UTC', NOW()) as _AIRBYTE_NORMALIZED_AT,
     _AIRBYTE_EXCHANGE_RATES_HASHID
 from raw.Observation
